5f1ede02240b8dce0954557d24c5c18470260f91
Iftekhar Fahim <iftekhar.fahim97@gmail.com>
2021-11-21 11:36:18 +0600

    ViewModels Added

---
+++ Restaurent-Apps/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Restaurent_Apps.ViewModel
+{
+    public class CustomerViewModel
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+    }
+}
---
+++ Restaurent-Apps/ViewModel/PaymentViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Restaurent_Apps.ViewModel
+{
+    public class PaymentViewModel
+    {
+        public int PaymentTypeId { get; set; } 
+        public string PaymentTypeName { get; set; }
+    }
+}
