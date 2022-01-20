2dcdba020f859cd6b4318d66a77564d34f508c47
Iftekhar Fahim <iftekhar.fahim97@gmail.com>
2022-01-20 19:15:50 +0600

    Problem Solved

--- Restaurent-Apps/Controllers/HomeController.cs
+++ Restaurent-Apps/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
 using System.Web.Mvc;
 using Restaurent_Apps.Repositories;
 
---
+++ Restaurent-Apps/ViewModel/OrderViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Restaurent_Apps.ViewModel
+{
+    public class OrderViewModel
+    {
+        public int OrderId { get; set; }
+        public int PaymentTypeId { get; set; }
+        public int CustomerId { get; set; }
+        public int OrderNumber { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal Final_Total { get; set; }
+    }
+}
